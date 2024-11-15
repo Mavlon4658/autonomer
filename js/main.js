04f580575851454b7eb48d2b409fd9dfa3781cc4
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-11-15 17:30:42 +0500

    footer and contact in progress

--- js/main.js
+++ js/main.js
@@ -5,3 +5,14 @@ bars.onclick = () => {
     blockTop.classList.toggle('active');
     bars.classList.toggle('active');
 }
+
+
+let footerTitle = document.querySelector('.footer__bottom-title');
+let footerRight = document.querySelector('.footer__bottom-right');
+
+footerTitle.addEventListener('click', () => {
+    footerRight.classList.toggle('active');
+    footerTitle.classList.toggle('active');
+})
+
+
